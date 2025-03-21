57db17b807bdb29075cd25c46ff63bfbded08b31
Javier Escobar <107713929+jescobarperez@users.noreply.github.com>
2025-03-21 12:22:58 -0400

    actualizando

--- asi_custom_sale/models/res_partner.py
+++ asi_custom_sale/models/res_partner.py
@@ -11,7 +11,8 @@ class ResPartner(models.Model):
     accreditation_sign_invoices_data = fields.Text(string='Datos de Acreditación para firmar facturas')
     bank_accounts = fields.Text(compute='_compute_bank_accounts', store=True)
     asi_contract = fields.Char('Contract')
-    
+
+
     @api.depends('bank_ids','bank_ids.acc_holder_name','bank_ids.currency_id','bank_ids.acc_number')
     def _compute_bank_accounts(self):
         for partner in self:
@@ -20,3 +21,21 @@ class ResPartner(models.Model):
                 bank_account = f"No: {bank.acc_number}, Bco: {bank.bank_id.name}, Titular: {bank.acc_holder_name} Moneda: {bank.currency_id.name}"
                 bank_accounts.append(bank_account)
             partner.bank_accounts = ',- '.join(bank_accounts)
+            
+    @api.constrains('can_sign_invoices', 'accreditation_sign_invoices_data')
+    def _check_accreditation_sign_invoices_data(self):
+        for record in self:
+            if record.can_sign_invoices and not record.accreditation_sign_invoices_data:
+                raise ValidationError(
+                    "El campo 'Datos de Acreditación para firmar facturas' es obligatorio cuando 'Puede Firmar Facturas' está activado."
+                )
+
+    @api.onchange('can_sign_invoices')
+    def _onchange_can_sign_invoices(self):
+        if self.can_sign_invoices and not self.accreditation_sign_invoices_data:
+            return {
+                'warning': {
+                    'title': "Advertencia",
+                    'message': "El campo 'Datos de Acreditación para firmar facturas' es obligatorio cuando 'Puede Firmar Facturas' está activado.",
+                }
+            }            
--- asi_custom_sale/models/sale_order.py
+++ asi_custom_sale/models/sale_order.py
@@ -1,10 +1,30 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     project_responsible = fields.Many2one('res.users', string='Responsable del Proyecto')
-    asi_sign = fields.Binary(string='Firma ASI')
-    client_sign = fields.Binary(string='Firma del Cliente')
-    customer_signer_id = fields.Many2one('res.partner', 'Firmada por', domain="[('parent_id', '=', partner_id)]", tracking=True) 
+
+    customer_signer_id = fields.Many2one(
+        'res.partner',
+        string="Firmante del Cliente",
+        domain="[('can_sign_invoices', '=', True), ('parent_id', '=', partner_id)]",
+        help="Persona autorizada para firmar facturas por parte del cliente.")
     responsible_employee = fields.Many2one('res.users', string='Firmado por:', default=lambda self: self.env.user.sale_team_id.user_id.parent_id)
+
+
+    @api.onchange('partner_id')
+    def _onchange_partner_id(self):
+        if self.partner_id:
+            return {
+                'domain': {
+                    'customer_signer_id': [
+                        ('can_sign_invoices', '=', True),
+                        ('parent_id', '=', self.partner_id.id)
+                    ]
+                }
+            }
+        else:
+            return False
+
+
