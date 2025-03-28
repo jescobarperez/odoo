7b8448f870d9b11766b40a625b93e26159fb4825
Javier Escobar <107713929+jescobarperez@users.noreply.github.com>
2025-03-27 22:02:11 -0400

    adding sign

---
+++ l10n_cu_base_sign/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Firma Digital para Cuba (Certificados P12)",
+    "version": "16.0.1.0.0",
+    "author": "Tu Nombre",
+    "summary": "Módulo para gestionar certificados P12 y firmar documentos electrónicamente.",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_users_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
